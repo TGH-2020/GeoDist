5bb0e0f7a8e608d22d87f8b9e57b20c344acaf6e
TGH-2020 <60321567+TGH-2020@users.noreply.github.com>
2021-01-15 10:51:29 +0100

    Create code.R

---
+++ code.R
@@ -0,0 +1,21 @@
+# Load libraries "geosphere" and "ape".
+library(geosphere)
+library(ape)
+
+# Read file.
+Coord <- read.csv(file="../Languages.csv", header=TRUE, encoding="UTF-8")
+
+# Create matrix with all longitudes followed by all latitudes, format it for the "distm" function from the geosphere package.
+Coord_mat <- matrix(c(Coord$longitude, Coord$latitude), ncol=2)
+
+# Create matrix of pairwise geographical distances between all corrdinates
+GeoDist_Mat <- round((distm(Coord_mat, fun=distVincentyEllipsoid)/1000), digits=2) 
+
+# Assign the doculect names to the matrix
+dimnames(GeoDist_Mat) <- list(Coord$name, Coord$name) 
+
+# Tree inferred with balanced NJ-algorithm. "Ladderize" re-arranges the tree. 
+GeoDist_Tree <- ladderize(nj(GeoDist_Mat)) 
+
+# Transform data of type "phylo" into a Newick file. " file="" " displays the Newick string in the console, adding a path saves it as '.nwk'.
+write.tree(ExampleTree, file = "", append = FALSE, digits = 10, tree.names = FALSE)
